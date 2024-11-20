c71303fc2a9fc440c715c2528a0f1f4079e3ba2d
Anu <77046926+anupriyasingh317@users.noreply.github.com>
2024-11-20 17:36:13 +0530

    Delete Mortgage directory

--- Mortgage/run_process_loan_data.sql
+++
@@ -1,7 +0,0 @@
-DECLARE
-    v_status VARCHAR2(4000);
-BEGIN
-    process_loan_data(p_file_name => 'loan_data_file.csv', p_mapping_file => 'mapping_file.csv', p_status => v_status);
-    DBMS_OUTPUT.PUT_LINE(v_status);
-END;
-/
